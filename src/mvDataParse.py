cf25ae644b616274b8b1e7aaf9964bc0551a29c7
edyoung75 <edyoung75ny@gmail.com>
2020-06-08 10:42:23 -0400

    Initial Pipeline created CSV --> Python --> Parquet

--- src/mvDataParse.py
+++ src/mvDataParse.py
@@ -1,9 +1,23 @@
+'''
+Insight Data Engineering Project
+Version 0.0.01
+
+Contact:
+Edmund Young
+dryoung@solidstate.dev
+
+Purpose:
+Move Automated Manifest System files to S3 Bucket
+
+Columns:
+'''
+
 import io
 import boto3
 import time
 import json
-import ssdpackagefunctions as fn
-
+#import ssdpackagefunctions as fn
+import mvDataParseFunctions as fn
 
 # Timer
 nStartTime = time.time()   # Time start
@@ -16,20 +30,25 @@ nStartTime = time.time()   # Time start
 # Lists:
 tmpList = []
 
-
-# Open a connect a connection to S3
+print('hello')
+# Create Client connections
+dx = boto3.client('dataexchange', region_name='us-east-1')
+#s3 = boto3.client('s3')
 s3 = boto3.resource('s3')
 
+# Define IDs to use
+dataSetId  = 'd0e9bd6148e8f14889980954017b0927'
+revisionId = '9f8ccb4b471f642909582c248d65afeb'
+
 # Connect to Data Exchange
-client = boto3.client('dataexchange')
-response = client.get_asset(
-    DataSetId='d0e9bd6148e8f14889980954017b0927',
-    RevisionId='9f8ccb4b471f642909582c248d65afeb',
+response = dx.get_asset(
+    DataSetId=dataSetId,
+    RevisionId=revisionId,
     AssetId='e0625157f2be4fadb77a064202f56e42'
 )
 
 # returns a Dict of the Data Set
-response = client.get_data_set(DataSetId='d0e9bd6148e8f14889980954017b0927')
+response = dx.get_data_set(DataSetId=dataSetId)
 # type(response)
 
 #print(list(response.keys()))
@@ -46,10 +65,10 @@ print(response.get('Id'))
 #print(response)
 
 # Paginator
-paginator = client.get_paginator('list_data_set_revisions')
+paginator = dx.get_paginator('list_data_set_revisions')
 
 responseIterator = paginator.paginate(
-    DataSetId='d0e9bd6148e8f14889980954017b0927',
+    DataSetId=dataSetId,
     PaginationConfig={
         'MaxItems': 123,
         'PageSize': 123
@@ -63,12 +82,35 @@ for page in responseIterator:
     revisionsList = sorted(revisionsList, key=lambda k: k['UpdatedAt'], reverse=True)
 
     for row in revisionsList:
-        print(row.keys())
+        #print(row.keys())
         print(row.get('UpdatedAt'))
         print(row.get('DataSetId'))
         print(row.get('Id'))
         print(row.get('Finalized'))
-        print('\n')
+
+# Returns a list of asset dictionaries
+assetList = fn.getAllRevisionAssets(dataSetId,revisionId)
+assetList = sorted(assetList, key=lambda k: k['UpdatedAt'], reverse=True)
+# Keys: ['Arn', 'AssetDetails', 'AssetType', 'CreatedAt', 'DataSetId', 'Id', 'Name', 'RevisionId', 'UpdatedAt']
+# AssetDetails has a nested dict like: {'S3SnapshotAsset': {'Size': 624028320}}
+for row in assetList:
+    #print(row.keys())
+    print(row.get('UpdatedAt'))
+    print(row.get('DataSetId'))
+    print(row.get('RevisionId'))
+    print(row.get('Id'))
+    print(row.get('Name'))
+    print(row.get('AssetType'))
+    print(row.get('AssetDetails'))
+    print(row.get('Arn'))
+    print('\n')
+
+revisionList = dx.list_revision_assets(
+        DataSetId=dataSetId,
+        RevisionId=revisionId
+    )
+#print(revisionList.keys())
+print('\n')
 
 # Print out the bucket names
 for bucket in s3.buckets.all():
